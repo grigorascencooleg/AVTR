37a65f7a17378fad830961763c65b77ae7531ef8
Oleg Grigorascenco <grigorascenco.oleg@elev.cihcahul.md>
2022-02-19 17:14:22 +0200

    Final /w/ adaptive

--- js/main.js
+++ js/main.js
@@ -5,5 +5,15 @@ $(function(){
         variableWidth: true,
         prevArrow: '<img class="arrow arrow-left" src="./images/arrow-left.svg">',
         nextArrow: '<img class="arrow arrow-right" src="./images/arrow-right.svg">',
+        responsive:[
+            {
+                breakpoint: 361,
+                settings:{
+                    variableWidth: false,
+                    slidesToShow: 1,
+                    arrows:false
+                } 
+            }
+        ]
    });
 });
